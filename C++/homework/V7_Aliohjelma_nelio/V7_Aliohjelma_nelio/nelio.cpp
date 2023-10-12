d888c4eee124f9ae34351098298947fb2e4a2ed1
Heejeong Han <heejeong.han@tuni.fi>
2023-10-13 00:06:16 +0300

    Add nelio

---
+++ C++/homework/V7_Aliohjelma_nelio/V7_Aliohjelma_nelio/nelio.cpp
@@ -0,0 +1,27 @@
+//
+//  nelio.cpp
+//  V7_Aliohjelma_nelio
+//
+//  Created by Heejeong Han on 12.10.2023.
+//
+
+#include <iostream>
+#include <cmath>
+
+void Nelio (){
+    
+    int luku ( 0 );
+    std::cout << "Anna luku, jonka nelio lasketaan: ";
+    std::cin >> luku;
+    
+    int nelio;
+    std::cout << "Luvun nelio on: ";
+    
+}
+
+int main (){
+    Nelio();
+    return 0;
+}
+
+
