44697974cdf11479d27cf3c9f3dc3490518a0787
Heejeong Han <heejeong.han@tuni.fi>
2023-10-13 14:15:07 +0300

    nelio update

--- C++/homework/V7_Aliohjelma_nelio/V7_Aliohjelma_nelio/nelio.cpp
+++ C++/homework/V7_Aliohjelma_nelio/V7_Aliohjelma_nelio/nelio.cpp
@@ -8,20 +8,20 @@
 #include <iostream>
 #include <cmath>
 
+//create a function
 void Nelio (){
     
     int luku ( 0 );
     std::cout << "Anna luku, jonka nelio lasketaan: ";
     std::cin >> luku;
     
-    int nelio;
-    std::cout << "Luvun nelio on: ";
-    
+    int p = 2;
+    int nelio = pow(luku,p);         // pow (n,p) = power function (value, powervalue)
+
+    std::cout << "Luvun nelio on: " << nelio;
 }
 
 int main (){
     Nelio();
     return 0;
 }
-
-
