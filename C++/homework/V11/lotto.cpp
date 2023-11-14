99991a8c0bd82005f1cd529cf8310e82b2d85c8c
Heejeong Han <heejeong.han@tuni.fi>
2023-11-14 09:40:03 +0200

    lotto semi done (maybe add getline)

--- C++/homework/V11/lotto.cpp
+++ C++/homework/V11/lotto.cpp
@@ -1,115 +1,123 @@
 // V11_3 Lotto
-
 #include <iostream>
 #include <array>
+#include <random>
+
 using namespace std;
 
 //maarittellaan loton arvoa
 array <int,7> lottonum = {2, 3, 10, 26, 29, 32, 37};
 array <int,3> bonusnum = {11, 17, 27};
 
+
 // konen random numero generaattori
-std::random_device loto;
-std::default_random_engine lottori { loto{} };
-std::uniform_int_distribution kone {1, 100};
-array <int, 7> konelotto, konebonus { kone (lottori) };
+random_device rd;
+uniform_int_distribution <int> randomi {1,100};
 
 // tulosta voittonumeroita
 void voittonumerot (){
-    cout << "\nJa voittonumerot ovat... ";
-    for (size_t i = 0; i < lottonum.size (); i++){cout << lottonum [i] << ", ";} 
-    cout << " ja bonusnumerot ";
-    for (size_t i = 0; i < bonusnum.size ()-1; i++){cout << bonusnum [i] << ", ";} 
+    cout << "\nJa voittonumerot ovat... "; 
+    for (int num : lottonum){cout << num << ", ";} 
+    cout << "ja bonusnumerot ";
+    for (int num = 0; num < bonusnum.size()-1; num++){cout << bonusnum[num] << ", ";}
+    cout << bonusnum [2] << ".\n"; 
 }
-
-// Lotto pelin toiminnot
+// Lotto pelin toiminnon valintaminen
 void toiminto (){
-    cout << "Mitä haluaisit tehdä? (Syötä numero 1-4)\n";
+    cout << "\nMitä haluaisit tehdä? (Syötä numero 1-4)\n";
     array <string, 4> optio = {
         "Katso palkintosummat", 
         "Pelaa koneen arpomalla rivillä", 
         "Pelaa omilla numeroilla", 
         "Lopeta pelaaminen"
-    }
-    for (int i = 0; i < 4; i++){cout << i+1 << ": " << optio [i];}
+    };
+    for (int i = 0; i < 4; i++){cout << i+1 << ": " << optio [i] << endl;}
 }
-
 // Palkintosummat taulukon tulostaminen (case 1)
 void tulostapalkkio (){
-    array <char, 8> num = {'7',"6+1",'6','5','4', "3+1",'2','1'};
+    array <string,8> num = {"7","6+1","6","5","4","3+1","2","1"}; 
     array <string,8> euro = {"10 000 000 e","1 000 000 e", "100 000 e", "2000 e", "50 e", "10 e", "2 e", "1 e"};
+    cout << "\nNumeroita + lisänumeroita oikein    " << "Voittosumma" << '\n';
+    for (int i = 0; i < num.size(); i++){
+        if (num[i].length() == 1){
+          cout << num [i] << "                                   " << euro [i] << '\n';} 
+        else {cout << num [i] << "                                 " << euro [i] << '\n';}
+        
+    }
+}
+
+// palkkio numeroiden tarkastelu ja tulostaminen
+void palkkio(array<int, 7>& lottoarvo, array<int, 3>& bonusarvo) {
+    long palkkioeuro = 0;
+    array<long, 8> euro = {1,2,10,50,2000,100000,1000000,10000000};
     
-    cout << "Numeroita + lisänumeroita oikein    " << "Voittosumma" << '\n';
-    for (size_t i = 0; i < num.size; i++){
-        cout << num[i] << "                                   " <<  << euro [i] << '\n';
+    int lottopointCount = 0;
+    for (int i = 0; i < lottoarvo.size(); ++i) {
+        for (int j = 0; j < lottonum.size(); ++j) {
+            if (lottoarvo[i] == lottonum[j]) {lottopointCount++;}
+        }
     }
-}
-// palkkion tulostaminen 
-int palkkio (int lottopoint, int bonuspoint){
-    int bonusnum = 1;
-    long palkkioeuro;
-    array <int, 7> lottonum = {7,6,5,4,3,2,1},
-    array <long,8> euro = {10000000,1000000,100000,2000,50,10,2,1};
-    if (lottopoint == lottonum [0]){palkkioeuro =  euro[0];}
-    else if (lottopoint == lottonum [1], bonuspoint == bonusnum){palkkioeuro = euro [1];}
-    else if (lottopoint == lottonum [2,3]){palkkioeuro = euro [2,3];}
-    else if (lottopoint == lottonum [4], bonuspoint == bonusnum){palkkioeuro =  euro [4];}
-    else if (lottopoint == lottonum [5,6]){palkkioeuro = euro [5,6];}
-    cout << "Sait " << lottotarkistus(lottopoint) << "+" << bonustarkistus(bonuspoint) " oikein!\n";
-    cout << "Olet voittanut " << palkkioeuro << " euroa!\n";
-}
-
-// lottonumeroiden tarkistaminen
-int lottotarkistus(array <int,7>& lottoarvo) {
-    int lottopoint = 0;
-    for (size_t i = 0; i < lottonum.size; i++){
-        if (lottoarvo[i] = lottonum[i]){
-            lottopoint++;} 
+    int bonuspointCount = 0;
+    for (int i = 0; i < bonusarvo.size(); ++i) {
+        for (int j = 0; j < bonusnum.size(); ++j){
+            if (bonusarvo[i] == bonusnum [j]) {bonuspointCount++;}
+        }
     }
-    return point;
+    // tulosta paljonko point saanut oikein
+    cout << "\nSait " << lottopointCount << "+" << bonuspointCount << " oikein!\n";
+    // tulosta saatava palkkio 
+    if (lottopointCount != 0) {
+        if (lottopointCount == 6 && bonuspointCount >= 1) {palkkioeuro = euro[6];}      // palkkio 6+1  
+        else if (lottopointCount == 3 && bonuspointCount >= 1) {palkkioeuro = euro [2];} // palkkio 3+1
+        else {palkkioeuro = euro[lottopointCount];};
+        cout << "Olet voittanut " << palkkioeuro << " euroa!\n\n";
+    } else {cout << "Voi harmi. Yrittäkääpäs toiseen kerran.\n";}    
 }
-
-// bonusnumeroiden tarkistaminen
-int bonustarkistus (array <int,7>& bonusarvo){
-    int bonuspoint = 0;
-    for (size_t i = 0; i < bonusnum.size; i++){
-        if (bonusarvo[i] = bonusnum[i]){
-            bonuspoint++;}   
-    }
-    return bonuspoint;
-}
-
 int main (){
-    cout << "Tervetuloa helppoon lottoon, voiton saaminen on helppoa!"
+    cout << "Tervetuloa helppoon lottoon, voiton saaminen on helppoa!\n";
 
     int toimitus; // toiminnon valitseminen syotto numero
-    array <int, 7> omalottonum, omabonusnum; // oma numerot (case 3)
-    toiminto ();
-    cin >> toimitus;
+    array <int, 7> konelotto, omalotto; // lottonumerot 
+    array <int, 3> konebonus, omabonus; // bonusnumerot 
 
     do {
-        tulostapalkkio ();
-        if (toimitus = 1){palkkio();}
-        else if (toimitus = 2){
-            // tulosta konearvo numerot
-            cout << "Lottorivisi on: ";
-            for (size_t i = 0; i < konelotto.size (); i++){cout << konelotto [i] << ", ";}  
-            cout << " ja bonusnumerot ";
-            for (size_t j = 0; j < konebonus.size ()-1; j++){cout << konebonus [j] << ", " }
-            cout << ".\n";
-            voittonumerot(); // tulosta voittonumeroita
-            // tarkista koneella arvastettu lottonumerot
-            palkkio (konelotto, konebonus);
-        } else if (tomitus = 3){
-            cout << "Lottorivisi on: ";
-            for (size_t i = 0; i < omalottonum.size (); i++){cin >> omalottonum [i] << ", ";}
-            cout << " ja bonusnumerot ";
-            for (size_t i = 0; i < omabonusnum.size ()-1; i++){cin >> omabonusnum [i] << ", ";}
-            cout << ".\n";
-            voittonumerot();
-            palkkio (omalottonum, omabonusnum); 
-        } 
+        toiminto();
+        cin >> toimitus; 
+        switch (toimitus){
+            case 1: 
+                tulostapalkkio(); 
+            break;
+
+            case 2: // koneella arvotettuna numerolla
+                // maarita konearvot
+                for (int& num : konelotto){num = randomi (rd);} 
+                for (int& num : konebonus){num = randomi (rd);} 
+                // tulosta konearvo numerot
+                cout << "\nLottorivisi on: ";
+                for (int num : konelotto){cout << num << ", ";}  
+                cout << "ja bonusnumerot ";
+                for (size_t num = 0; num < konebonus.size()-1; num++){cout << konebonus[num] << ", ";}  
+                cout << konebonus [2] << ".\n";
+                voittonumerot(); // tulosta voittonumeroita
+                palkkio (konelotto, konebonus); // tulosta arvastettu palkkioita
+            break;
+
+            case 3: // oma arvolla
+                cout << "\nLottorivisi on\n";
+                for (int& num : omalotto){cout << ":"; cin >> num;}
+                cout << "ja bonusnumerot\n";
+                for (size_t num = 0; num < omabonus.size(); num++){cout << ":"; cin >> omabonus[num];}
+                cout << "\n";
+                voittonumerot();
+                palkkio (omalotto, omabonus); 
+            break;
+
+            default:
+                break;
+        }
         cout << "--------------------------------------------------------------------------------\n";
-    } while (toiminto !=4)
-    cout << "\nTervetuloa uudestaan." << endl;
+    } while (toimitus != 4);
+
+    cout << "\nTervetuloa uudestaan.";
+    return 0;
 }
